e3c9ba45647f74ebed233019ccfd9ba74d85f315
masantillan <76527236+masantillan@users.noreply.github.com>
2026-06-22 12:54:51 -0300

    Add files via upload

---
+++ resources.py
@@ -0,0 +1,225 @@
+#Se importa el modulo sqlite3 y colorama e inicializamos
+import sqlite3
+from colorama import init, Fore, Back
+init()
+
+def startdb():
+    '''Conexión a la base de datos y creación en caso de no existir'''
+    #Se conecta y crea el cursor para la DB
+    conexion = sqlite3.connect("inventario.db")
+
+    cursor = conexion.cursor()
+
+    #Se crea la tabla de no existir
+    cursor.execute('''
+                CREATE TABLE IF NOT EXISTS inventario (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nombre TEXT NOT NULL,
+                        descripcion TEXT,
+                        cantidad INTEGER NOT NULL,
+                        precio REAL NOT NULL,
+                        categoria TEXT
+                )
+    ''')
+
+    #Guardamos los cambios y cerramos la conexion
+    conexion.commit()
+    conexion.close()
+
+def mainmenu():
+    '''Creación del menú principal'''
+    #Mostramos el menu principal
+    print(Fore.GREEN + "-"*23)
+    print(Fore.GREEN + " Sistema de Inventario ")
+    print(Fore.GREEN + "-"*23)
+    print(Fore.WHITE + "1. Ingreso de Productos")
+    print(Fore.WHITE + "2. Productos en Stock")
+    print(Fore.WHITE + "3. Actualización de Productos")
+    print(Fore.WHITE + "4. Eliminación de Productos")
+    print(Fore.WHITE + "5. Búsqueda de Productos")
+    print(Fore.WHITE + "6. Stock Bajo")
+    print(Fore.WHITE + "7. Salir")
+
+def add(nombre, desc, cantidad, precio, cate):
+    '''Agregamos nuevos productos con Nombre, Descripción (opcional), Cantidad, Precio y Categoria (opcional)'''
+    #Se conecta y crea el cursor para la DB
+    conexion = sqlite3.connect("inventario.db")
+
+    cursor = conexion.cursor()
+
+    #Ingresamos los datos cargados a la DB y ordenamos en la tabla
+    try:
+        cursor.execute('''BEGIN TRANSACTION''')
+
+        cursor.execute('''
+                    INSERT INTO inventario (nombre, 
+                       descripcion, 
+                       cantidad, 
+                       precio, 
+                       categoria)
+                    VALUES (?, ?, ?, ?, ?)
+                    ''', (nombre, desc, cantidad, precio, cate))
+    
+        conexion.commit()
+        print(f"Producto '{nombre}' cargado exitosamente")
+    #En caso de haber algun dato erroneo o invalido, se deshacen los cambios realizados
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print("Error en la carga")
+    #Cerramos la conexion al terminar la transaccion
+    finally:
+        conexion.close()
+
+def stock():
+    '''Revisa la base de datos y muestra todos los productos en existencia junto a su stock'''
+    #Se conecta y crea el cursor para la DB
+    conexion = sqlite3.connect("inventario.db")
+
+    cursor = conexion.cursor()
+
+    #Seleccionamos todos los datos del inventario
+    try:
+        cursor.execute('''BEGIN TRANSACTION''')
+
+        cursor.execute('''
+                       SELECT * FROM inventario
+                       ''')
+        invprod = cursor.fetchall()
+        if not invprod:
+                print(Fore.RED + "No hay productos cargados.")
+                return
+        #Mostramos el stock
+        print("\n ---Listado de Productos---")
+        for producto in invprod:
+            print(f"ID: {producto[0]}, Nombre: {producto[1]}, Cantidad: {producto[3]}, Descripción: {producto[2]}, Precio: {producto[4]}, Categoria: {producto[5]}")
+        conexion.commit()
+    #En caso de error, se deshace cualquier cambio
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print("Error al buscar productos")
+    #Se cierra la conexion
+    finally:
+        conexion.close()
+
+def act(prodid, nameact=None, descact=None, cantact=None, precioact=None, catact=None):
+    '''Actualizamos el precio del producto en base a su ID'''
+    #Se conecta y crea el cursor para la DB
+    conexion = sqlite3.connect("inventario.db")
+
+    cursor = conexion.cursor()
+# Buscar producto actual
+    cursor.execute(
+        "SELECT nombre, descripcion, cantidad, precio, categoria FROM inventario WHERE id = ?",
+            (prodid,)
+        )
+
+    producto = cursor.fetchone()
+
+    if producto is None:
+        print(f"No existe un producto con ID {prodid}")
+        return
+
+    # Mantener valor actual si viene vacío
+    nombre = nameact if nameact not in ("", None) else producto[0]
+    descripcion = descact if descact not in ("", None) else producto[1]
+    cantidad = cantact if cantact not in ("", None) else producto[2]
+    precio = precioact if precioact not in ("", None) else producto[3]
+    categoria = catact if catact not in ("", None) else producto[4]    
+
+    #Ingresamos los nuevos datos a la tabla
+    try:
+        cursor.execute('''BEGIN TRANSACTION''')
+
+        cursor.execute('''UPDATE inventario SET nombre = ?, 
+                       descripcion = ?, 
+                       cantidad = ?, 
+                       precio = ?, 
+                       categoria = ? 
+                       WHERE id = ?''', (nombre, descripcion, cantidad, precio, categoria, prodid))
+        conexion.commit()
+
+        print(f"Producto ID: {prodid} actualizado correctamente")
+
+        #Realizamos la verificación para ver si se actualizó
+        cursor.execute('''SELECT * FROM inventario WHERE id = ?''', (prodid,))
+        prodact = cursor.fetchone()
+
+        print("\n Producto actualizado")
+        print(f"ID: {prodact[0]} | Nombre: {prodact[1]} | Cantidad: {prodact[2]} | Descripción: {prodact[3]} | Precio: {prodact[4]:2f} | Categoria: {prodact[5]}")
+    #En caso de error, se deshace cualquier cambio
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print("Error al buscar productos")
+    #Se cierra la conexion
+    finally:
+        conexion.close()
+
+def wipe(prodid):
+    '''Borramos un producto y toda su información, buscando el mismo mediante su ID'''
+    conexion = sqlite3.connect("inventario.db")
+
+    cursor = conexion.cursor()
+
+    try:
+        cursor.execute('''BEGIN TRANSACTION''')
+
+        cursor.execute('''
+                       DELETE FROM inventario WHERE id = ?
+                       ''', (prodid,))
+        conexion.commit()
+        print(f"Se eliminó el producto: {prodid} de la base de datos")
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print("Error al actualizar")
+    finally:
+        conexion.close()
+
+def look(prodid):
+    '''Busqueda de productos en relación a su ID'''
+    conexion = sqlite3.connect("inventario.db")
+
+    cursor = conexion.cursor()
+
+    try:
+        cursor.execute('''BEGIN TRANSACTION''')
+
+        cursor.execute('''
+                       SELECT * FROM inventario WHERE id = ?
+                       ''', (prodid,))
+        prodact = cursor.fetchone()
+
+        if prodact is None:
+            print(Fore.RED + f"No existe producto con el ID: {prodid}")
+            return
+        else:
+            print(f"ID: {prodact[0]} | Nombre: {prodact[1]} | Cantidad: {prodact[3]} | Descripción: {prodact[2]} | Precio: {prodact[4]:2f} | Categoria: {prodact[5]}")
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print(f"Error en la búsqueda {e}")
+    finally:
+        conexion.close()
+
+def lowstock():
+    '''Mostramos los productos que tengan stock menor a 3 unidades al momento'''
+    conexion = sqlite3.connect("inventario.db")
+
+    cursor = conexion.cursor()
+
+    try:
+        cursor.execute('''BEGIN TRANSACTION''')
+
+        cursor.execute('''
+                       SELECT * FROM inventario WHERE cantidad < ?
+                       ''', (3,))
+        bajostock = cursor.fetchall()
+        if not bajostock:
+                print(Fore.RED + "No hay productos con stock bajo.")
+                return
+        print("\nProductos con Stock Bajo")
+        for producto in bajostock:
+            print(f"ID: {producto[0]} | Nombre: {producto[1]} | Cantidad: {producto[3]} | Descripción: {producto[2]} | Precio: {producto[4]:2f} | Categoria: {producto[5]}")
+    except sqlite3.Error as e:
+        conexion.rollback()
+        print("Error al mostrar la opción seleccionada")
+    finally:
+        conexion.close()
---
+++ tfi.py
@@ -0,0 +1,56 @@
+#Importamos las funciones requeridas para que el programa tenga todas sus funciones
+import resources as r
+
+#Importamos colorama
+from colorama import init, Fore, Back
+init()
+
+#Inicializamos la DB y creamos la tabla
+r.startdb()
+
+while True: 
+    #Mostramos el menu y elegimos la opción
+    r.mainmenu()
+
+    seleccion = input("Elije una opción: ").strip()
+
+    if seleccion.isdigit(): #Verificación para la opción
+        seleccion = int(seleccion)
+        match seleccion:
+            case 1: #Se ingresan los datos para la carga del producto
+                nombre = input("Ingrese el nombre del producto: ")
+                if nombre != "":
+                    descripcion = input("Ingrese la descripción: ")
+                    cantidad = input("Ingrese la cantidad actual: ")
+                    precio = input("Ingrese el precio: ")
+                    categoria = input("Ingrese la categoria: ")
+                    r.add(nombre, descripcion, cantidad, precio, categoria)
+                else:
+                    print(Fore.RED + "\n'Nombre' no puede estar vacío")
+                    continue
+            case 2: #Mostramos todos los productos disponibles
+                r.stock()
+            case 3: #Actualizamos el producto en base a su ID
+                prodid = int(input("Ingrese el ID del producto a actualizar: "))
+                print(Fore.RED + "Para mantener un valor, dejar vacío el campo" + Fore.WHITE)
+                nameact = input("Ingrese el nuevo nombre: ").strip() or None
+                descact = input("Ingrese nueva descripción: ").strip() or None
+                cantact = input("Ingrese nueva cantidad: ").strip() or None
+                precioact = input("Ingrese el nuevo precio: ").strip() or None
+                catact = input("Ingrese nueva categoría: ").strip() or None
+                r.act(prodid, nameact, descact, cantact, precioact,catact)
+            case 4: #Se elimina el producto en base a su ID
+                prodid = int(input("Ingrese el ID del producto a eliminar: "))
+                r.wipe(prodid)
+            case 5: #Buscamos un producto en base a su ID
+                prodid = int(input("Ingrese el ID del producto a buscar: "))
+                r.look(prodid)
+            case 6: #Traemos los productos, en este caso con stock menor a 3
+                r.lowstock()
+            case 7: #Salimos del programa
+                input("\nEl programa se cerrará...\n")
+                break
+            case _: #Mensaje de error en caso de que el numero no corresponda
+                print(Fore.RED + "\nOpción invalida")
+    else: #Mensaje de error en caso de que no sea un numero
+        print(Fore.RED + "\nOpción invalida, seleccione un número")
