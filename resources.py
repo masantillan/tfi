5a2d602ef45affa93af7a8f991d244b578e57801
masantillan <smfacundo96@gmail.com>
2026-06-27 13:45:48 -0300

    feat: update resources.py 27/6

--- resources.py
+++ resources.py
@@ -91,10 +91,10 @@ def stock():
         #Mostramos el stock
         print("\n---Listado de Productos---")
         for producto in invprod:
-            print("="*25)
+            print(Fore.BLUE + "="*25)
             print(f"ID: {producto[0]}") 
             print("="*25)
-            print(f"| Nombre: {producto[1]}\n| Cantidad: {producto[3]}\n| Descripción: {producto[2]}\n| Precio: {producto[4]}\n| Categoria: {producto[5]}")
+            print(Fore.MAGENTA + f"| Nombre: {producto[1]}\n| Cantidad: {producto[3]}\n| Descripción: {producto[2]}\n| Precio: {producto[4]}\n| Categoria: {producto[5]}")
         conexion.commit()
     #En caso de error, se deshace cualquier cambio
     except sqlite3.Error as e:
@@ -148,10 +148,10 @@ def act(prodid, nameact=None, descact=None, cantact=None, precioact=None, catact
         prodact = cursor.fetchone()
 
         print("\n Producto actualizado")
-        print("="*25)
+        print(Fore.BLUE + "="*25)
         print(f"ID: {producto[0]}") 
         print("="*25)
-        print(f"| Nombre: {producto[1]}\n| Cantidad: {producto[3]}\n| Descripción: {producto[2]}\n| Precio: {producto[4]}\n| Categoria: {producto[5]}")
+        print(Fore.MAGENTA + f"| Nombre: {producto[1]}\n| Cantidad: {producto[3]}\n| Descripción: {producto[2]}\n| Precio: {producto[4]}\n| Categoria: {producto[5]}")
     #En caso de error, se deshace cualquier cambio
     except sqlite3.Error as e:
         conexion.rollback()
@@ -198,11 +198,11 @@ def look(prodid):
             print(Fore.RED + f"No existe producto con el ID: {prodid}")
             return
         else:
-                    print("\n Producto actualizado")
-        print("="*25)
-        print(f"ID: {prodact[0]}") 
-        print("="*25)
-        print(f"|Nombre: {prodact[1]}\n| Cantidad: {prodact[3]}\n| Descripción: {prodact[2]}\n| Precio: {prodact[4]}\n| Categoria: {prodact[5]}")
+            print("\n Producto actualizado")
+            print(Fore.BLUE + "="*25)
+            print(f"ID: {prodact[0]}") 
+            print("="*25)
+            print(Fore.MAGENTA + f"| Nombre: {prodact[1]}\n| Cantidad: {prodact[3]}\n| Descripción: {prodact[2]}\n| Precio: {prodact[4]}\n| Categoria: {prodact[5]}")
     except sqlite3.Error as e:
         conexion.rollback()
         print(f"Error en la búsqueda {e}")
@@ -227,11 +227,10 @@ def lowstock():
                 return
         print("\nProductos con Stock Bajo")
         for producto in bajostock:
-            print("\n Producto actualizado")
-            print("="*25)
+            print(Fore.BLUE + "="*25)
             print(f"ID: {producto[0]}") 
             print("="*25)
-            print(f"| Nombre: {producto[1]}\n| Cantidad: {producto[3]}\n| Descripción: {producto[2]}\n| Precio: {producto[4]}\n| Categoria: {producto[5]}")
+            print(Fore.MAGENTA + f"| Nombre: {producto[1]}\n| Cantidad: {producto[3]}\n| Descripción: {producto[2]}\n| Precio: {producto[4]}\n| Categoria: {producto[5]}")
     except sqlite3.Error as e:
         conexion.rollback()
         print("Error al mostrar la opción seleccionada")
